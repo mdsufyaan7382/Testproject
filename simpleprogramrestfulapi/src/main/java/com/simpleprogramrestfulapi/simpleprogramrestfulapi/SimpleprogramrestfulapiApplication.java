6ca0bfe08e7e10cc2ffe9255d0d64f6cd9dff87a
Sufyaan Khan <Sufyaan Khan@DESKTOP-78HNIBP>
2021-08-02 11:11:07 +0530

    First commit

---
+++ simpleprogramrestfulapi/src/main/java/com/simpleprogramrestfulapi/simpleprogramrestfulapi/SimpleprogramrestfulapiApplication.java
@@ -0,0 +1,13 @@
+package com.simpleprogramrestfulapi.simpleprogramrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleprogramrestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleprogramrestfulapiApplication.class, args);
+	}
+
+}
---
+++ simpleprogramrestfulapi/src/main/java/com/simpleprogramrestfulapi/simpleprogramrestfulapi/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.simpleprogramrestfulapi.simpleprogramrestfulapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simpleprogramrestfulapi.simpleprogramrestfulapi.models.SampleResponse;
+
+@RestController
+
+public class WebController {
+	
+	@RequestMapping("/sample")
+	public SampleResponse Sample(@RequestParam(value = "name",
+	defaultValue = "Robot") String name) {
+		SampleResponse response = new SampleResponse();
+		response.setId(1);
+		response.setMessage("Your name is "+name);
+		return response;
+	}
+
+
+}
